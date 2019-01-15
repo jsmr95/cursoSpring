e93b1359596e08a32b49102cbf8a2801b59f791c
jsmr95 <josemaria.gallego@iesdonana.org>
2019-01-15 11:26:22 +0100

    Modifico proyecto y creo el archivo beans.xml

---
+++ HelloWorldSpring/src/main/java/com/openwebinars/hellospring/App.java
@@ -0,0 +1,5 @@
+package com.openwebinars.hellospring;
+
+public class App {
+
+}
---
+++ HelloWorldSpring/src/main/java/com/openwebinars/hellospring/Mundo.java
@@ -0,0 +1,19 @@
+package com.openwebinars.hellospring;
+
+public class Mundo {
+
+    private String saludo;
+    
+    public Mundo(String saludo) {
+    	this.saludo = saludo;
+    }
+
+    public String getSaludo() {
+        return saludo;
+    }
+
+    public void setSaludo(String saludo) {
+        this.saludo = saludo;
+    }
+
+}
